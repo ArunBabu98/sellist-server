0f9d23e224ba449d05a1c339d3a48e8cab2b006a
Arun Xena <arun.b@tenshilabs.com>
2026-02-02 10:37:18 +0530

    .env added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const app = require("./src/app");
 const config = require("./src/config");
 const logger = require("./src/config/logger.config");
 const { getLocalIP } = require("./src/utils/helpers");
+require("dotenv").config();
 
 // Graceful shutdown handler
 function gracefulShutdown(signal, server) {
